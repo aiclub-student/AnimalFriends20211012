46fa2d5397e2e4a2475dc2320452ff8a54685e4b
Amit Gupta <amit@gprof.com>
2021-10-13 12:44:01 -0700

    Initial Commit

---
+++ AnimalFriends20211012/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AnimalFriends20211012
+//
+//  Created by Amit Gupta on 10/13/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
